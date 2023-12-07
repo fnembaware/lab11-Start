0da78a93833652acea7c1e18268a98e0d0ac2f68
Fadzai Emmah <fadzai@tlu.ee>
2023-12-07 22:23:40 +0200

    Initial commit

---
+++ meal.js
@@ -0,0 +1,8 @@
+//1- Link to get a random meal
+//https://www.themealdb.com/api/json/v1/1/random.php
+
+//2- Link to lookup a specific meal with an id
+//https://www.themealdb.com/api/json/v1/1/lookup.php?i=
+
+//3- Link to search for meals using a keyword
+//https://www.themealdb.com/api/json/v1/1/search.php?s=
